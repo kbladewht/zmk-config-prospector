2838cdad9c91e6d301ddcd6b4665b73cb01c0a57
kbladewht <w00110601111@163.com>
2026-09-21 23:40:13 +0800

    Update c.sh

--- c.sh
+++ c.sh
@@ -23,7 +23,6 @@ west build -p always -s zmk/app -b kblade//zmk -- \
 -DEXTRA_CONF_FILE="D:/project/GitHub/zmk-config-prospector/config/prospector_scanner.conf"; 
 
       
-
 SRC_FILE="build/zephyr/zmk.uf2"
 # Generate filename with keyword, e.g., flash_left.uf2, flash_right.uf2, flash_dongle.uf2
 DEST_FILE="${DRIVE_P}:/flash${FILE_SUFFIX}.uf2"
